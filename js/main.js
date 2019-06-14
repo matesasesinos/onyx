e1e5d26a324632d8bba439edd72e06c2754392e1
jeiriart <jeiriart@gmail.com>
2019-06-14 20:36:43 -0300

    agregada ventana ADD modal

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 // JavaScript Document
-(function($){
+(function ($) {
 	"use strict";
-	$(document).ready(function(){
-		$('#password-view').on('click touchstart', function(){
+	$(document).ready(function () {
+		$('#password-view').on('click touchstart', function () {
 			var pass = $('#password').attr('type');
-			if(pass === 'password'){
+			if (pass === 'password') {
 				$('#password').attr('type', 'text');
 				$('#password-view').removeClass('password-view').addClass('password-no-view');
 			}
-			if(pass === 'text'){
+			if (pass === 'text') {
 				$('#password').attr('type', 'password');
 				$('#password-view').removeClass('password-no-view').addClass('password-view');
 			}
 		});
 	});
-	$(document).ready(function(){
+	$(document).ready(function () {
 		$('#menu-reponsive').hide();
-		$('.menu-link').on('click touchstart', function(){
+		$('.menu-link').on('click touchstart', function () {
 			$('#menu-reponsive').toggle('slide');
 			$(this).hide();
 		});
-		$('.close-button').on('click touchstart',function(){
+		$('.close-button').on('click touchstart', function () {
 			$('#menu-reponsive').hide('fast');
 			$('.menu-link').show('slow');
 		});
 	});
-	
-	$(document).ready(function(){
-		
+
+	$(document).ready(function () {
+
 		if ($('input#hour-cb').is(':checked') && $('input#hour-cb').val(1)) {
 			$('span.tgl-white').show();
 			$('span.tlg-black').hide();
@@ -35,8 +35,8 @@
 			$('span.tgl-white').hide();
 			$('span.tlg-black').show();
 		}
-		
-		$('.tgl').on('click', function(){
+
+		$('.tgl').on('click', function () {
 			if ($('input#hour-cb').is(':checked') && $('input#hour-cb').val(1)) {
 				$('span.tgl-white').show();
 				$('span.tlg-black').hide();
@@ -50,7 +50,17 @@
 			}
 		});
 	});
-	
+
+	$(document).ready(function () {
+		$('#add-lookup-modal').hide();
+		$('.content-add').on('click', function(){
+			$('#add-lookup-modal').slideToggle('fast');
+		});
+		$('.close-lookup-modal').on('click', function(){
+			$('#add-lookup-modal').slideToggle('fast');
+		});
+	});
+	/*
 	function readURL(input) {
             if (input.files && input.files[0]) {
                 var reader = new FileReader();
@@ -62,5 +72,5 @@
 
                 reader.readAsDataURL(input.files[0]);
             }
-        }
+        }*/
 })(jQuery);
