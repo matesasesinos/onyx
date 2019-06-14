052b362cf83d3c4ec48f3498afb8f2ff170211c9
jeiriart <jeiriart@gmail.com>
2019-06-14 09:51:51 -0300

    menu

--- js/main.js
+++ js/main.js
@@ -14,4 +14,27 @@
 			}
 		});
 	});
+	$(document).ready(function(){
+		$('#menu-reponsive').hide();
+		$('.menu-link').on('click touchstart', function(){
+			$('#menu-reponsive').toggle('slide');
+			$(this).hide();
+		});
+		$('.close-button').on('click touchstart',function(){
+			$('#menu-reponsive').hide('fast');
+			$('.menu-link').show('slow');
+		});
+	});
+	function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#photo')
+                        .attr('src', e.target.result);
+                };
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
 })(jQuery);
